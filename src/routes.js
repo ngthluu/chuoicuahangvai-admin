d2ea48092544e09a9c68bb890e70a0ca6b3a4f30
ngthluu <ngthluu211@gmail.com>
2021-12-03 00:35:15 +0700

    Init branch #1

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 
 const BranchHome = React.lazy(() => import('./views/branches/Home'))
 const BranchTrash = React.lazy(() => import('./views/branches/Trash'))
+const BranchAdd = React.lazy(() => import('./views/branches/Add'))
 
 const WarehouseInventory = React.lazy(() => import('./views/warehouses/Inventory'))
 const WarehouseImport = React.lazy(() => import('./views/warehouses/import/Home'))
@@ -29,30 +30,31 @@ const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
 
-  { path: '/branches', name: 'Chi nhánh', component: BranchHome },
   { path: '/branches/trash', name: 'Thùng rác', component: BranchTrash },
+  { path: '/branches/add', name: 'Thêm chi nhánh', component: BranchAdd },
+  { path: '/branches', name: 'Chi nhánh', component: BranchHome },
 
-  { path: '/warehouses', exact: true, name: 'Kho hàng' },
   { path: '/warehouses/inventory', name: 'Tồn kho', component: WarehouseInventory },
   { path: '/warehouses/import', name: 'Nhập kho', component: WarehouseImport },
   { path: '/warehouses/import/trash', name: 'Thùng rác', component: WarehouseImportTrash },
   { path: '/warehouses/export', name: 'Xuất kho', component: WarehouseExport },
   { path: '/warehouses/export/trash', name: 'Thùng rác', component: WarehouseExportTrash },
+  { path: '/warehouses', exact: true, name: 'Kho hàng' },
 
-  { path: '/products', name: 'Sản phẩm', component: ProductHome },
   { path: '/products/trash', name: 'Thùng rác', component: ProductTrash },
+  { path: '/products', name: 'Sản phẩm', component: ProductHome },
 
-  { path: '/orders', exact: true, name: 'Đơn hàng' },
   { path: '/orders/sell', name: 'Đơn hàng bán', component: OrderSell },
   { path: '/orders/refund', name: 'Đơn hàng trả', component: OrderRefund },
+  { path: '/orders', exact: true, name: 'Đơn hàng' },
 
-  { path: '/users', name: 'Nhân viên', component: UserHome },
   { path: '/users/trash', name: 'Thùng rác', component: UserTrash },
   { path: '/users/salary', name: 'Bảng lương', component: UserSalary },
+  { path: '/users', name: 'Nhân viên', component: UserHome },
 
-  { path: '/customers', name: 'Khách hàng', component: CustomerHome },
   { path: '/customers/trash', name: 'Thùng rác', component: CustomerTrash },
   { path: '/customers/debt', name: 'Công nợ', component: CustomerDebt },
+  { path: '/customers', name: 'Khách hàng', component: CustomerHome },
 ]
 
 export default routes
---
+++ src/views/branches/Add.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { CCard, CCardBody, CCol, CRow, CCardHeader } from '@coreui/react'
+
+const Add = () => {
+  return (
+    <CRow>
+      <CCol xs={8}>
+        <CCard className="mb-4">
+          <CCardHeader>
+            <h5>Thêm chi nhánh</h5>
+          </CCardHeader>
+          <CCardBody> </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  )
+}
+
+export default Add
