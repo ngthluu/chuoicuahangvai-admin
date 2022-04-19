a6386c9411234fc2c347fa106fe327430df9b311
Luu <ngthluu@gmail.com>
2022-04-19 16:45:27 +0700

    Finish refund

--- src/views/orders/refund/ViewInvoice.js
+++ src/views/orders/refund/ViewInvoice.js
@@ -32,27 +32,32 @@ const ViewInvoice = () => {
   const query = useLocation().search
   const id = new URLSearchParams(query).get('id')
 
+  const [invoiceCode, setInvoiceCode] = useState('')
+  const [orderId, setOrderId] = useState('')
   const [orderCode, setOrderCode] = useState('')
   const [createdTime, setCreatedTime] = useState('')
-  const [branchName, setBranchName] = useState('')
 
-  const [email, setEmail] = useState('')
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [phone, setPhone] = useState('')
+  const [address, setAddress] = useState('')
+  const [ward, setWard] = useState('')
+  const [district, setDistrict] = useState('')
+  const [city, setCity] = useState('')
 
   const [products, setProducts] = useState([])
-  const [note, setNote] = useState('')
-  const [statuses, setStatuses] = useState([])
+
+  const [invoiceTotal, setInvoiceTotal] = useState('')
 
   const fetchData = async () => {
     if (id === null) return
     const query = qs.stringify(
       {
         populate: [
-          'customer',
-          'customer.name',
-          'branch',
+          'refund',
+          'customer_name',
+          'receive_address',
+          'receive_address.address_three_levels',
           'products',
           'products.sku',
           'products.sku.product',
@@ -61,24 +66,29 @@ const ViewInvoice = () => {
           'products.sku.width',
           'products.sku.origin',
           'products.sku.images',
-          'refund_statuses',
-          'refund_statuses.update_user',
         ],
       },
       { encodeValuesOnly: true },
     )
     const response = await axios.get(`
-      ${process.env.REACT_APP_STRAPI_URL}/api/refunds/${id}?${query}`)
+      ${process.env.REACT_APP_STRAPI_URL}/api/refund-invoices/${id}?${query}`)
     const data = response.data.data
 
-    setOrderCode(`REFUND#${data.id}`)
+    setInvoiceCode(`R-INVOICE#${data.id}`)
+    setInvoiceTotal(data.attributes.price)
+    setOrderId(data.attributes.refund.data.id)
+    setOrderCode(`REFUND#${data.attributes.refund.data.id}`)
     setCreatedTime(data.attributes.createdAt)
-    setBranchName(data.attributes.branch.data.attributes.name)
 
-    setEmail(data.attributes.customer.data.attributes.email)
-    setPhone(data.attributes.customer.data.attributes.phone)
-    setFirstName(data.attributes.customer.data.attributes.name.firstname)
-    setLastName(data.attributes.customer.data.attributes.name.lastname)
+    setFirstName(data.attributes.customer_name.firstname)
+    setLastName(data.attributes.customer_name.lastname)
+    setPhone(data.attributes.customer_phone)
+    if (data.attributes.receive_address) {
+      setAddress(data.attributes.receive_address.address)
+      setWard(data.attributes.receive_address.address_three_levels.data.attributes.ward)
+      setDistrict(data.attributes.receive_address.address_three_levels.data.attributes.district)
+      setCity(data.attributes.receive_address.address_three_levels.data.attributes.city)
+    }
 
     setProducts(
       data.attributes.products.map((item) => {
@@ -93,8 +103,6 @@ const ViewInvoice = () => {
         }
       }),
     )
-    setNote(data.attributes.note)
-    setStatuses(data.attributes.refund_statuses.data)
   }
 
   useEffect(() => {
@@ -106,19 +114,19 @@ const ViewInvoice = () => {
       <CCol md={12}>
         <CCard className="mb-4">
           <CCardHeader>
-            <h5>Thông tin đơn trả hàng</h5>
+            <h5>Thông tin đơn trả</h5>
           </CCardHeader>
           <CCardBody>
             <CRow>
               <CCol md={5} className="mb-3">
-                <h6 className="mb-3">Thông tin đơn trả hàng</h6>
+                <h6 className="mb-3">Thông tin hóa đơn</h6>
                 <div className="d-flex justify-content-between mb-3">
-                  <div>Mã đơn trả: </div>
-                  <div>{orderCode}</div>
+                  <div>Mã hóa đơn: </div>
+                  <div>{invoiceCode}</div>
                 </div>
                 <div className="d-flex justify-content-between mb-3">
-                  <div>Trả tại chi nhánh: </div>
-                  <div>{branchName}</div>
+                  <div>Mã đơn trả: </div>
+                  <Link to={`/orders/refund/view?id=${orderId}`}>{orderCode}</Link>
                 </div>
                 <div className="d-flex justify-content-between mb-3">
                   <div>Thời gian khởi tạo: </div>
@@ -127,11 +135,7 @@ const ViewInvoice = () => {
               </CCol>
               <CCol md={2}></CCol>
               <CCol md={5} className="mb-3">
-                <h6 className="mb-3">Thông tin người trả</h6>
-                <div className="d-flex justify-content-between mb-3">
-                  <div>Email người trả: </div>
-                  <div>{email}</div>
-                </div>
+                <h6 className="mb-3">Thông tin người mua</h6>
                 <div className="d-flex justify-content-between mb-3">
                   <div>Họ và tên: </div>
                   <div>{`${firstName} ${lastName}`}</div>
@@ -140,6 +144,14 @@ const ViewInvoice = () => {
                   <div>Số điện thoại: </div>
                   <div>{phone}</div>
                 </div>
+                {ward !== '' ? (
+                  <div className="d-flex justify-content-between mb-3">
+                    <div>Địa chỉ nhận hàng: </div>
+                    <div>{`${address}, ${ward}, ${district}, ${city}`}</div>
+                  </div>
+                ) : (
+                  <></>
+                )}
               </CCol>
             </CRow>
             <CRow className="mb-3">
@@ -191,52 +203,13 @@ const ViewInvoice = () => {
                         })()}
                       </CTableHeaderCell>
                     </CTableRow>
-                  </CTableFoot>
-                </CTable>
-              </CCol>
-            </CRow>
-            <CRow className="mb-3">
-              <CCol md={12}>
-                <CFormLabel>Ghi chú</CFormLabel>
-                <CFormTextarea
-                  placeholder="Nhập ghi chú"
-                  rows="5"
-                  value={note}
-                  onChange={(e) => setNote(e.target.value)}
-                ></CFormTextarea>
-                <CFormFeedback invalid>Không hợp lệ!</CFormFeedback>
-              </CCol>
-            </CRow>
-            <CRow className="mb-3">
-              <CCol md={12}>
-                <CFormLabel>Lịch sử cập nhật</CFormLabel>
-                <CTable align="middle" bordered>
-                  <CTableHead align="middle" color="info">
                     <CTableRow>
-                      <CTableHeaderCell scope="col"> # </CTableHeaderCell>
-                      <CTableHeaderCell scope="col"> Thời gian </CTableHeaderCell>
-                      <CTableHeaderCell scope="col"> Người cập nhật </CTableHeaderCell>
-                      <CTableHeaderCell scope="col"> Trạng thái </CTableHeaderCell>
+                      <CTableHeaderCell colSpan="5"> Giá trị hóa đơn </CTableHeaderCell>
+                      <CTableHeaderCell scope="col">
+                        {invoiceTotal.toLocaleString()}
+                      </CTableHeaderCell>
                     </CTableRow>
-                  </CTableHead>
-                  <CTableBody align="middle">
-                    {statuses.map((item, index) => (
-                      <CTableRow key={index}>
-                        <CTableDataCell> {index + 1} </CTableDataCell>
-                        <CTableDataCell>
-                          {!item.attributes.update_status ? (
-                            <CBadge color="warning">Chưa xác nhận</CBadge>
-                          ) : (
-                            <CBadge color="success">Đã xác nhận và nhập kho</CBadge>
-                          )}
-                        </CTableDataCell>
-                        <CTableDataCell>
-                          {item.attributes.update_user.data.attributes.email}
-                        </CTableDataCell>
-                        <CTableDataCell>{item.attributes.update_time}</CTableDataCell>
-                      </CTableRow>
-                    ))}
-                  </CTableBody>
+                  </CTableFoot>
                 </CTable>
               </CCol>
             </CRow>
