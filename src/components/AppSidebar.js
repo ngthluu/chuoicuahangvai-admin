5ae6ecb6f83c73e1d0e18d02608298014d5be50e
Luu <ngthluu@gmail.com>
2022-04-23 15:38:57 +0700

    Fix a bug

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 
-import { CSidebar, CSidebarBrand, CSidebarNav, CSidebarToggler } from '@coreui/react'
+import { CSidebar, CSidebarBrand, CSidebarNav } from '@coreui/react'
 
 import { AppSidebarNav } from './AppSidebarNav'
 
@@ -13,19 +13,11 @@ import navigation from '../_nav'
 import { Link } from 'react-router-dom'
 
 const AppSidebar = () => {
-  const dispatch = useDispatch()
   const unfoldable = useSelector((state) => state.sidebarUnfoldable)
   const sidebarShow = useSelector((state) => state.sidebarShow)
 
   return (
-    <CSidebar
-      position="fixed"
-      unfoldable={unfoldable}
-      visible={sidebarShow}
-      onVisibleChange={(visible) => {
-        dispatch({ type: 'set', sidebarShow: visible })
-      }}
-    >
+    <CSidebar position="fixed" unfoldable={unfoldable} visible={sidebarShow}>
       <Link style={{ color: 'inherit', textDecoration: 'none' }} to={'/dashboard'}>
         <CSidebarBrand className="d-none d-md-flex">
           <h1> CHV </h1>
